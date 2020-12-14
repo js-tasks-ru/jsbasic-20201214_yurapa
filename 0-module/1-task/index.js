1d6e34f89a831fc9fffdf57cf7a24a77e40a8a1d
yurapa <yura.paray@gmail.com>
2020-12-14 21:59:42 +0200

    Sum function

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -2,8 +2,25 @@
  * sum
  * @param {number} m
  * @param {number} n
- * @returns {number}
+ * @returns {number, boolean}
  */
 function sum(m, n) {
-  // ваш код...
+  if (Number.isFinite(m) && Number.isFinite(n)) {
+    return m + n;
+  }
+
+  return false;
 }
+
+console.log(sum('test string', 33));
+console.log(sum(33));
+console.log(sum(33,));
+console.log(sum(2.2, 3.1));
+console.log(sum(2, 33));
+console.log(sum(NaN, 33));
+console.log(sum(-NaN, 3));
+console.log(sum(23, 22));
+console.log(sum(13, 2));
+console.log(sum(Infinity, 22));
+console.log(sum(-Infinity, 22));
+console.log(sum(' === '));
