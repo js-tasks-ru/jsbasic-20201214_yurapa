963b23ad38753e7511091040d55f353efa32bcbf
yurapa <yura.paray@gmail.com>
2020-12-16 00:10:03 +0200

    updated after feedback

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -2,25 +2,17 @@
  * sum
  * @param {number} m
  * @param {number} n
- * @returns {number, boolean}
+ * @returns {number}
+ * @throws Will throw an error if one of the arguments is not a number.
  */
 function sum(m, n) {
-  if (Number.isFinite(m) && Number.isFinite(n)) {
-    return m + n;
+  if (!Number.isFinite(m)) {
+    throw new Error(` "m" is not a number!`);
   }
 
-  return false;
-}
+  if (!Number.isFinite(n)) {
+    throw new Error(` "n" is not a number!`);
+  }
 
-// console.log(sum('test string', 33));
-// console.log(sum(33));
-// console.log(sum(33,));
-// console.log(sum(2.2, 3.1));
-// console.log(sum(2, 33));
-// console.log(sum(NaN, 33));
-// console.log(sum(-NaN, 3));
-// console.log(sum(23, 22));
-// console.log(sum(13, 2));
-// console.log(sum(Infinity, 22));
-// console.log(sum(-Infinity, 22));
-// console.log(sum(' === '));
+  return m + n;
+}
