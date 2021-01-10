67e0b6e7ec9011039cabdd4502145f99f03f71d1
yurapa <yura.paray@gmail.com>
2021-01-10 23:11:33 +0200

    module 4 - task 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,9 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    if (table.rows[i].cells[i]) {
+      table.rows[i].cells[i].style.backgroundColor = 'red';
+    }
+  }
 }
