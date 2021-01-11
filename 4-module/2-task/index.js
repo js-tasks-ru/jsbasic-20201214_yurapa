5971b735922eddff743af9b659eb22dff46f1440
yurapa <yura.paray@gmail.com>
2021-01-11 16:34:45 +0200

    module 4 - task 2 - reworked to forEach

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,9 +3,9 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
-  for (let i = 0; i < table.rows.length; i++) {
-    if (table.rows[i].cells[i]) {
-      table.rows[i].cells[i].style.backgroundColor = 'red';
+  [...table.rows].forEach((item, i) => {
+    if (item.cells[i]) {
+      item.cells[i].style.backgroundColor = 'red';
     }
-  }
+  });
 }
