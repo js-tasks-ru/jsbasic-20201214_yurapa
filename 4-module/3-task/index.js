4bc8ab190180e24af7982b5fcf5fbd56c6db55b7
yurapa <yura.paray@gmail.com>
2021-01-17 16:05:25 +0200

    task 4.3 - feedback

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -7,46 +7,50 @@ function highlight(table) {
   let indexGender;
   let indexStatus;
 
-  [...table.rows[0].cells].forEach((_heading, i) => {
-    if (_heading.textContent === 'Age') {
-      indexAge = i;
-    } else if (_heading.textContent === 'Gender') {
-      indexGender = i;
-    } else if (_heading.textContent === 'Status') {
-      indexStatus = i;
-    }
-  });
+  const assignIndex = () => {
+    [...table.rows[0].cells].forEach((_heading, i) => {
+      if (_heading.textContent === 'Age') {
+        indexAge = i;
+      } else if (_heading.textContent === 'Gender') {
+        indexGender = i;
+      } else if (_heading.textContent === 'Status') {
+        indexStatus = i;
+      }
+    });
+  };
 
-  [...table.rows].forEach((_row, i) => {
+  assignIndex();
 
-    if (indexStatus && _row.cells[indexStatus].dataset.available) {
-      switch (_row.cells[indexStatus].dataset.available) {
+  [...table.rows].forEach((row, i) => {
+
+    if (indexStatus && row.cells[indexStatus].dataset.available) {
+      switch (row.cells[indexStatus].dataset.available) {
         case 'true':
-          _row.classList.add('available');
+          row.classList.add('available');
           break;
         case 'false':
-          _row.classList.add('unavailable');
+          row.classList.add('unavailable');
           break;
       }
     }
 
-    if (i > 0 && indexStatus && !_row.cells[indexStatus].dataset.available) {
-      _row.hidden = true;
+    if (i > 0 && indexStatus && !row.cells[indexStatus].dataset.available) {
+      row.hidden = true;
     }
 
     if (indexGender) {
-      switch (_row.cells[indexGender].textContent) {
+      switch (row.cells[indexGender].textContent) {
         case 'm':
-          _row.classList.add('male');
+          row.classList.add('male');
           break;
         case 'f':
-          _row.classList.add('female');
+          row.classList.add('female');
           break;
       }
     }
 
-    if (indexAge && (parseInt(_row.cells[indexAge].textContent)) < 18) {
-      _row.style.textDecoration = 'line-through';
+    if (indexAge && (parseInt(row.cells[indexAge].textContent)) < 18) {
+      row.style.textDecoration = 'line-through';
     }
   });
 }
